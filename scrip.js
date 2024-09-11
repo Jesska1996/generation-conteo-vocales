bcfcdc5ee9a84f6105525740159b5c9984215378
Karen Silvae <jessilva135@gmail.com>
2024-09-11 09:27:26 -0600

    feat:lectura del texto en el input nombre

--- scrip.js
+++ scrip.js
@@ -1,1 +1,26 @@
 console.log  (conteo de vocales)
+/**
+ Encuentra el número de letras que existen en el
+ nombre
+ @letra {string} letra a buscar
+ @nombre {string} texto a buscar la letra
+ return número de letras encontradas
+*/
+function encontrarNumeroDeLetra( letra, nombre ) {    
+    let numeroDeLetras = 0;
+    for(let index = 0; index < nombre.length; index++ ) {
+        let letraAEvaluar = nombre.charAt( index );
+        if( letraAEvaluar === letra){
+            numeroDeLetras++;
+        }
+    }
+    return numeroDeLetras;
+}
+fuction manejoDelEventoDeInputNombre (){
+   const inputNombre = document.getElementById ("nombre")
+    return inputNombre.value;
+}
+fuction manejoDelEventoDeInputNombre ()}
+    const nombre = leerValorDeNombre ();
+    console.log( nombre );
+}
