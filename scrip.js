a39d08c18db7f96e1cb6f10a07e9089f6ddc6c79
KAREN <jessilva135@gmail.com>
2024-10-17 21:08:59 -0600

    cambie archivos

--- scrip.js
+++ scrip.js
@@ -1,35 +1,41 @@
-console.log("Conteo de vocales");
-
-/**
- Encuentra el número de letras que existen en el
- nombre indicado.
- @letra {string} letra a buscar
- @nombre {string} texto a buscar la letra
- @return número de letras encontradas
-*/
-function encontrarNumeroDeLetra(letra, nombre) {    
-    let numeroDeLetras = 0;
-    for(let index = 0; index < nombre.length; index++ ) {
-        let letraAEvaluar = nombre.charAt(index);
-        if(letraAEvaluar === letra){
-            numeroDeLetras++;
-        }
-    }
-    return numeroDeLetras;
-}
-
-function leerValorDeNombre(){
-    const inputNombre = document.getElementById("nombre");
-    return inputNombre.value;
-}
-
-function imprimirResultado(resultado) {
-    const paragraph = document.getElementById("vocales"); // Cambio de 'documento' a 'document'
-    paragraph.innerHTML = resultado;
-}
-
-function manejoDelEventoDeInputNombre() {
-    const nombre = leerValorDeNombre().toLowerCase(); // Cambio de 'toLoweCase' a 'toLowerCase'
-    const numLetraA = encontrarNumeroDeLetra("a", nombre); // Corrección de sintaxis
-    imprimirResultado("Num letras A: " + numLetraA);
-}
+//cambie var por const y puse getElementary, además que vincule todo con la id
+document.addEventListener('DOMContentLoaded', () => {
+    const listaInvitados = document.getElementById("listaInvitados");
+    const nombreInvitado = documento.getElementById ("nombreInvitado");
+    const edadInvitado = documento.getElementById ("edadInvitado");  
+    const nacionalidadInvitado  = documento.getElementById ("nacionalidadInvitado");
+    const invitadoForm = document.getElementById("invitadoForm");
+  
+  // Agregar invitado a la lista
+      invitadoForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+  
+      const nombre = nombreInvitado.value.trim();
+      const edad = edadInvitado.value.trim(); 
+      const nacionalidad = nacionalidadInvitado.value();
+  
+      if (nombre === '' || edad === '' || nacionalidad === '') {
+        alert('Por favor, llena todos los campos');
+        return;
+      }
+  
+      const li = document.createElement('li');
+      li.classList.add ('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center');
+      li.innerHTML = `
+        ${nombre} (${edad} años) - ${nacionalidad}
+       <button class="btn btn-danger btn-sm eliminar">Eliminar</button>
+      `; 
+    
+    // Agregar el nuevo invitado a la lista
+    
+      listaInvitados.appendChild(li);
+      nombreInvitado.value = ''; // Limpiar campo de nombre
+      nacionalidadInvitado.value = ''; // Limpiar campo de nacionalidad
+    });
+     // Eliminar invitado de la lista
+     listaInvitados.addEventListener('click', (e) => {
+      if (e.target.classList.contains('eliminar')) {
+        e.target.parentElement.remove();
+      }
+    });
+  });
